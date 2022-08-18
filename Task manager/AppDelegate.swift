a04ffa2f43ef3613df2bb6f71ff62b9706d37c38
Vladimir Beliakov <belyakov1991@me.com>
2022-08-18 22:05:50 +0300

    Added 3 sec delay for Launch screen display.

--- Task manager/AppDelegate.swift
+++ Task manager/AppDelegate.swift
@@ -14,8 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 3.0)                                                                                  // Delay for Launch Screen display.
         return true
     }
     
